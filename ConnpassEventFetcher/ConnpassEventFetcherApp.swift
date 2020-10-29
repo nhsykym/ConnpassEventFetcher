62258793d638185528be8a9e3c247460d5745ced
Naohisa Yokoyama <nhsykym@gmail.com>
2020-10-29 21:27:42 +0900

    Initial Commit

---
+++ ConnpassEventFetcher/ConnpassEventFetcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConnpassEventFetcherApp.swift
+//  ConnpassEventFetcher
+//
+//  Created by 横山尚久 on 2020/10/29.
+//
+
+import SwiftUI
+
+@main
+struct ConnpassEventFetcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
